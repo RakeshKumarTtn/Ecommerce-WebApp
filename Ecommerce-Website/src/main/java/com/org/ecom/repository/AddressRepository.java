0a00fc3408ec0249e0dd807152cc7ee2fafbff74
RakeshKumarTtn <rakesh.kumar3@tothenew.com>
2022-11-20 20:01:36 +0530

    Added Some Code

--- Ecommerce-Website/src/main/java/com/org/ecom/repository/AddressRepository.java
+++
@@ -1,7 +0,0 @@
-package com.org.ecom.repository;
-
-import com.org.ecom.entities.Address;
-import org.springframework.data.repository.CrudRepository;
-
-public interface AddressRepository extends CrudRepository<Address, Long> {
-}
