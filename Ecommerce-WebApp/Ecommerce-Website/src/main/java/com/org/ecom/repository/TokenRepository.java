3933d0cdd1af58782a125def675cb03d364dad0c
RakeshKumarTtn <rakesh.kumar3@tothenew.com>
2022-11-20 19:58:20 +0530

    Revert "Added Some Code"
    This reverts commit 5dc25467db53664f4835c143020c81a84fc4a95b.

--- Ecommerce-WebApp/Ecommerce-Website/src/main/java/com/org/ecom/repository/TokenRepository.java
+++
@@ -1,8 +0,0 @@
-package com.org.ecom.repository;
-
-import com.org.ecom.entities.Token;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TokenRepository extends JpaRepository<Token,Integer> {
-
-}
